46462d42f90533493b1545c0182b636f647d8c46
Shasa <alifiashasa30@gmail.com>
2026-05-05 22:06:31 +0700

    feat: add Log Kesehatan module and configure associated routes

--- mobile/lib/app/routes/app_pages.dart
+++ mobile/lib/app/routes/app_pages.dart
@@ -6,6 +6,8 @@ import '../modules/dashboard/bindings/dashboard_binding.dart';
 import '../modules/dashboard/views/dashboard_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/log-kesehatan/bindings/log_kesehatan_binding.dart';
+import '../modules/log-kesehatan/views/log_kesehatan_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
 import '../modules/patient_detail/bindings/patient_detail_binding.dart';
@@ -54,6 +56,11 @@ class AppPages {
       page: () => const CalendarView(),
       binding: CalendarBinding(),
       transition: Transition.noTransition,
+    ),
+    GetPage(
+      name: _Paths.LOG_KESEHATAN,
+      page: () => const LogKesehatanView(),
+      binding: LogKesehatanBinding(),
     ),
   ];
 }
--- mobile/lib/app/routes/app_routes.dart
+++ mobile/lib/app/routes/app_routes.dart
@@ -10,6 +10,7 @@ abstract class Routes {
   static const PATIENT_DETAIL = _Paths.PATIENT_DETAIL;
   static const DASHBOARD = _Paths.DASHBOARD;
   static const CALENDAR = _Paths.CALENDAR;
+  static const LOG_KESEHATAN = _Paths.LOG_KESEHATAN;
 }
 
 abstract class _Paths {
@@ -21,4 +22,5 @@ abstract class _Paths {
   static const PATIENT_DETAIL = '/patient-detail';
   static const DASHBOARD = '/dashboard';
   static const CALENDAR = '/calendar';
+  static const LOG_KESEHATAN = '/log-kesehatan';
 }
