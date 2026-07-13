11e8901291c296397e56b6b4c70dd3efead087aa
Wahyu Akhmad Fadillah <wahyufadil1140@gmail.com>
2026-07-13 18:34:12 +0700

    Revert "feat(web): add privacy policy page"
    This reverts commit 83b922a991889eafde8fae426bcbed1ef70df9ab.

--- web-admin/src/main.tsx
+++ web-admin/src/main.tsx
@@ -3,19 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'next-themes'
 import './index.css'
 import App from './App.tsx'
-import PrivacyPage from './PrivacyPage.tsx'
-
-const isPrivacyPage = window.location.pathname.replace(/\/+$/, '') === '/privacy'
-
-if (isPrivacyPage) {
-  document.documentElement.lang = 'id'
-  document.title = 'Kebijakan Privasi | Smart Caregiver'
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {isPrivacyPage ? <PrivacyPage /> : <App />}
+      <App />
     </ThemeProvider>
   </StrictMode>,
 )
